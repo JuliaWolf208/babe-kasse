10ac9a8e28529ca62ce699c8c577caa1859fdfab
JuliaWolf208 <Julia2008@yahoo.de>
2026-05-03 15:19:12 +0200

    Add network-first service worker to prevent stale PWA cache

---
+++ sw.js
@@ -0,0 +1,24 @@
+// Service Worker – immer neueste Version vom Netzwerk laden
+const CACHE = 'babe-kasse-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
